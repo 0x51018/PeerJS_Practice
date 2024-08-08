071de8215143a7b9c70de8f547f38ab6f18bd75c
0x51018 <104045531+0x51018@users.noreply.github.com>
2024-08-08 20:30:36 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PeerJS_Chatting/',
+  base: '/p2p-chatting/',
   plugins: [react()],
 })
